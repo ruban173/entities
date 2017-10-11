6daa9d28ee6ad937142bae01cfaac881c90e14b2
ruban <ruban1703@gmail.com>
2017-10-12 00:39:50 +0300

    старт

---
+++ EntitiesFrameworkProject/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesFrameworkProject
+{
+    class User
+    {
+        public int id { get; set; }
+        public string login { get; set; }
+        public string password { get; set; }
+        public string access { get; set; }
+        public DateTime date { get; }
+        public Boolean status { get; set; }
+
+
+
+    }
+}
---
+++ EntitiesFrameworkProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesFrameworkProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (UserContext db = new UserContext())
+            {
+                // создаем два объекта User
+                User user1 = new User { login = "Tom"};
+                db.Users.Add(user1);
+                db.Users.Add(user1);
+                db.SaveChanges();
+                Console.WriteLine("Объекты успешно сохранены");
+
+                // получаем объекты из бд и выводим на консоль
+                var result = db.Users;
+                Console.WriteLine("Список объектов:");
+                foreach (User u in result)
+                {
+                    Console.WriteLine("{0}.{1} - {2}", u.id, u.login, u.password);
+                }
+            }
+            Console.Read();
+        }
+    }
+}
